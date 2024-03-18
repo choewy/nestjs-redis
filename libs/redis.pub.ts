a78e07f014d1223e52914d18b63bcadb46214db5
choewy <choewy32@gmail.com>
2024-03-18 18:11:07 +0900

    feature/redis pub

--- libs/redis.pub.ts
+++ libs/redis.pub.ts
@@ -1,7 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Redis } from 'ioredis';
 
 @Injectable()
 export class RedisPub {
+  private readonly logger = new Logger(RedisPub.name);
+
   constructor(private readonly redis: Redis) {}
+
+  async publish(channel: string | Buffer, message: string | Buffer) {
+    if (channel instanceof Buffer) {
+      channel = channel.toString('utf-8');
+    }
+
+    if (message instanceof Buffer) {
+      message = message.toString('utf-8');
+    }
+
+    const subscribers = await this.redis.publish(channel, message);
+
+    this.logger.debug(
+      JSON.stringify(
+        {
+          message: 'succeed publish message',
+          subscribers,
+          target: {
+            channel,
+            message,
+          },
+        },
+        null,
+        2,
+      ),
+    );
+  }
 }
